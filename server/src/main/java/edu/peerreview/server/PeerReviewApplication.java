308675bd5a69288df2af757cf06d7bcbaeb825bc
Vladimir Gajčin <djvlada03@gmail.com>
2020-08-31 13:24:43 +0200

    Init projects + some schema

---
+++ server/src/main/java/edu/peerreview/server/PeerReviewApplication.java
@@ -0,0 +1,13 @@
+package edu.peerreview.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerReviewApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/edu/peerreview/server/PeerReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.peerreview.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
